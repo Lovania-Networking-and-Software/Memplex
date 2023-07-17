ba2babf380cb50d9c709e0ae1cc0b37225699d08
Desto2000 <nNn%&5sAu4FiJkrv@5$u@KSHHxbL5ui@x@783Z5R$B!5^gTPtztQ*@6&jPE4%z8q>
2023-07-17 21:59:22 +0300

    Linting and added docs

--- memplex/layers/__init__.py
+++ memplex/layers/__init__.py
@@ -12,4 +12,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # ===========================================================================
+
+"""
+Layers for Memplex main model
+"""
 from memplex.layers.labeler import Labeler
--- memplex/layers/labeler.py
+++ memplex/layers/labeler.py
@@ -16,12 +16,26 @@ import tensorflow as tf
 
 
 class RoundedNorm(tf.keras.layers.Wrapper):
+    """
+    Rounds and normalizes the data.
+    """
+
     def __init__(self, layer, name="rounded_norm"):
+        """
+        Initializes normalization layer.
+        :param layer: Layer to be wrapped
+        :param name: Name of the wrapper
+        """
         super().__init__(layer, name=name)
 
         self.norm = tf.keras.layers.UnitNormalization()
 
-    def call(self, inputs):
+    def call(self, inputs: tf.Tensor) -> tf.Tensor:
+        """
+        Rounds and normalizes the data.
+        :param inputs: Input tensor
+        :return: Output tensor
+        """
         return tf.round(
             self.norm(
                 self.layer(inputs)
@@ -30,7 +44,15 @@ class RoundedNorm(tf.keras.layers.Wrapper):
 
 
 class PoolingAndAverageBasedSpaceCreatorLayer(tf.keras.layers.Layer):
-    def __init__(self, name="space_creator"):
+    """
+    Pooles data with two methods, get averages of pools, and concatenates them.
+    """
+
+    def __init__(self, name: str = "space_creator"):
+        """
+        Initializes layers and base class.
+        :param name: Name of the layer
+        """
         super().__init__(False, name)
         self.pooling_max = tf.keras.layers.GlobalMaxPooling1D(name="pooling_space_max", keepdims=True)
         self.pooling_average = tf.keras.layers.GlobalAveragePooling1D(
@@ -43,7 +65,14 @@ class PoolingAndAverageBasedSpaceCreatorLayer(tf.keras.layers.Layer):
         self.average = tf.keras.layers.Average(name="space_average")
         self.concatenate = tf.keras.layers.Concatenate(name="space_concatenate")
 
-    def call(self, inputs):
+    def call(self, inputs: tf.Tensor) -> tf.Tensor:
+        """
+        Pools the data with 2 different methods, then averages the two different data from 2 different methods,
+        then sums them, then average the average and aggregated data and sums,
+        then again averages and sums the average and aggregated data, then combines them.
+        :param inputs: Input tensor
+        :return: Output tensor
+        """
         pooled_space_max = self.norm(self.pooling_max(inputs))
         pooled_space_average = self.norm(self.pooling_average(inputs))
 
@@ -63,7 +92,15 @@ class PoolingAndAverageBasedSpaceCreatorLayer(tf.keras.layers.Layer):
 
 
 class Labeler(tf.Module):
+    """
+    Labeles the data.
+    """
+
     def __init__(self, name="labeler"):
+        """
+        Initializes layers and base class.
+        :param name: Name of Labeler module
+        """
         super().__init__(name=name)
         self.space_creator = PoolingAndAverageBasedSpaceCreatorLayer()
         self.fourier_features = tf.keras.layers.experimental.RandomFourierFeatures(128, "gaussian", 4.,
@@ -71,9 +108,14 @@ class Labeler(tf.Module):
         self.linear = tf.keras.layers.PReLU(name="linear_space")
 
     def __call__(self, inputs):
-        attention_space = self.space_creator(inputs)
+        """
+        Creates super soft space with inputs, projects them to 128 dim feature space and applies linear activations.
+        :param inputs: Input tensor
+        :return: Output tensor
+        """
+        space = self.space_creator(inputs)
 
-        feature_space = tf.round(self.fourier_features(attention_space))
+        feature_space = tf.round(self.fourier_features(space))
 
         linear_data = self.linear(feature_space)
 
