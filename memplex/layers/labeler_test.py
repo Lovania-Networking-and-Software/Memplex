f06295576cd4259c191b8589180ffa9eac94a289
Desto2000 <nNn%&5sAu4FiJkrv@5$u@KSHHxbL5ui@x@783Z5R$B!5^gTPtztQ*@6&jPE4%z8q>
2023-07-17 20:39:00 +0300

    Bazel build system and data Labeler

---
+++ memplex/layers/labeler_test.py
@@ -0,0 +1,32 @@
+#  Copyright 2023 Lovania Networking and Software
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ===========================================================================
+import unittest
+from memplex import layers
+import numpy as np
+import tensorflow as tf
+
+
+class LabelerTest(unittest.TestCase):
+    def test_labeler(self):
+        labeler = layers.Labeler()
+        base_data = np.random.rand(3, 3, 3)
+        data1 = np.copy(base_data)
+        data2 = np.copy(base_data)
+        data2 += np.random.normal(0, 0.4, size=(3, 3, 3))
+        self.assertEqual(tf.reduce_mean(labeler(data1)), tf.reduce_mean(labeler(data2)))
+
+
+if __name__ == '__main__':
+    unittest.main()
