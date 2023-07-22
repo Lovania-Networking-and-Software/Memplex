f84e9b15448c966b8ccfc7a07a654dc8cd67092a
Desto2000 <nNn%&5sAu4FiJkrv@5$u@KSHHxbL5ui@x@783Z5R$B!5^gTPtztQ*@6&jPE4%z8q>
2023-07-22 20:48:32 +0300

    Memory added

--- memplex/utils/memory.py
+++ memplex/utils/memory.py
@@ -12,3 +12,130 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # ===========================================================================
+import sys
+from collections import deque
+from typing import Any, Iterator
+
+import tensorflow as tf
+from safetensors import safe_open
+from safetensors.tensorflow import save_file
+
+
+class Sizes:
+    KB: int = 1024
+    MB: int = 1024 * 1024
+    GB: int = 1024 * 1024 * 1024
+
+
+class Memory:
+    def __init__(self, memory_size_treshold: int = 1 * Sizes.GB,
+                 filename: str = "experience.safetensors"):
+        self._filename = filename
+        self._memory_size_treshold = memory_size_treshold
+        self._capacity = memory_size_treshold
+        self._queue = deque()
+        self._data = dict[str, tf.Tensor]()
+
+    def __getitem__(self, key) -> tf.Tensor:
+        with tf.GradientTape() as tp:
+            try:
+                if key in self._data.keys():
+                    self._queue.remove(key)
+                    self._queue.append(key)
+                res = self._data[key]
+            except KeyError:
+                try:
+                    _pre_data = {}
+                    with safe_open(self._filename, framework="tf") as f:
+                        for k in f.keys():
+                            _pre_data = f.get_tensor(k)
+                    res = _pre_data[key]
+                except FileNotFoundError or KeyError:
+                    res = None
+            return res
+
+    def __setitem__(self, key, value):
+        with tf.GradientTape() as tp:
+            try:
+                _pre_data = {}
+                with safe_open(self._filename, framework="tf") as f:
+                    for k in f.keys():
+                        _pre_data[k] = f.get_tensor(k)
+                _pre_data[key] = value
+                if key in self._data.keys():
+                    self._queue.remove(key)
+                self._queue.append(key)
+                self._data[key] = value
+                if sys.getsizeof(self._queue) > self._capacity:
+                    save_file(self._data, self._filename)
+                    del self._data[self._queue.popleft()]
+                save_file(_pre_data, self._filename)
+            except FileNotFoundError:
+                self._queue.append(key)
+                self._data[key] = value
+                if sys.getsizeof(self._queue) > self._capacity:
+                    save_file(self._data, self._filename)
+                    del self._data[self._queue.popleft()]
+                save_file(self._data, self._filename)
+
+    def __delitem__(self, key):
+        with tf.GradientTape() as tp:
+            try:
+                _pre_data = {}
+                with safe_open(self._filename, framework="tf") as f:
+                    for k in f.keys():
+                        _pre_data[k] = f.get_tensor(k)
+                del _pre_data[key]
+                del self._data[key]
+                self._queue.remove(key)
+                save_file(_pre_data, self._filename)
+            except FileNotFoundError:
+                del self._data[key]
+                self._queue.remove(key)
+                save_file(self._data, self._filename)
+            except KeyError:
+                raise KeyError
+
+    def __iter__(self) -> Iterator[Any] | Any:
+        try:
+            with tf.GradientTape() as tp:
+                _pre_data = {}
+                with safe_open(self._filename, framework="tf") as f:
+                    for k in f.keys():
+                        _pre_data[k] = f.get_tensor(k)
+                return iter(_pre_data)
+        except FileNotFoundError:
+            return iter(self._data)
+
+    def __len__(self) -> int:
+        try:
+            with tf.GradientTape() as tp:
+                _pre_data = {}
+                with safe_open(self._filename, framework="tf") as f:
+                    for k in f.keys():
+                        _pre_data[k] = f.get_tensor(k)
+                return len(_pre_data)
+        except FileNotFoundError:
+            return len(self._data)
+
+    def __str__(self) -> str:
+        try:
+            with tf.GradientTape() as tp:
+                _pre_data = {}
+                with safe_open(self._filename, framework="tf") as f:
+                    for k in f.keys():
+                        _pre_data[k] = f.get_tensor(k)
+                return str(_pre_data)
+        except FileNotFoundError:
+            return str(self._data)
+
+    def __repr__(self) -> str:
+        try:
+            with tf.GradientTape() as tp:
+                _pre_data = {}
+                with safe_open(self._filename, framework="tf") as f:
+                    for k in f.keys():
+                        _pre_data[k] = f.get_tensor(k)
+                return repr(_pre_data)
+        except FileNotFoundError:
+            return repr(self._data)
